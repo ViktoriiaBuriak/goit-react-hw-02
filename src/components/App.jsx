5b3a22c1f603907d1a943d5cfe76c7edb38ab1a7
ViktoriiaBuriak <143516734+ViktoriiaBuriak@users.noreply.github.com>
2024-03-10 08:39:24 +0100

    add structure

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import Description from "./Description/Description";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Description />
+    </>
+  );
+};
+
+export default App;
